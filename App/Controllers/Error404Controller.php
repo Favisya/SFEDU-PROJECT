129b21a9985e65febaca7d550385d7d5ad2b421f
Dmitriy Vikulin <dvikulin@lachestry.com>
2022-08-30 17:54:16 +0300

    Make error 404 controller
    [#599]

---
+++ App/Controllers/Error404Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers;
+
+class Error404Controller
+{
+    public function showControllerName()
+    {
+        echo 'This is controller for 404 error' . PHP_EOL;
+    }
+}
